50bcfe98d72cc855c92fdce88ee9c6fe49fb935e
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-06 13:57:51 -0800

    add ActivityCard Component

--- activities/src/App.js
+++ activities/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
+import ActivityCard from "./components/ActivityCard"
 class App extends Component {
  render() {
    return (
      <div className="App">
      <Navbar/>
+     <ActivityCard/>
      </div>
    );
  }
