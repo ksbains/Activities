568bb6b93b45485597db1b9a2e8bd2f4e40fd9c1
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-07 16:07:24 -0800

    fixed ActivityCard css

--- activities/src/components/ActivityCard/ActivityCard.js
+++ activities/src/components/ActivityCard/ActivityCard.js
@@ -11,19 +11,19 @@ render () {
       className="activity-card-container container"
     >
       <div className="row">
-        <div className="event-activity col-md-5 col-sm-5 col-lg-5">
+        <div className="event-activity col-md-4 col-sm-4 col-lg-4">
         <strong>Horse Golf</strong>
         </div>
-        <div className="user-created col-md-5 col-sm-5 col-lg-5">
-        <span>Created By [USERNAME]</span>
+        <div className="user-created col-md-4 col-sm-4 col-lg-4">
+        <span>[USERNAME]</span>
         </div>
-        <div className="profile-picture col-md-5 col-sm-5 col-lg-5">
-        <img src="https://thumbs.dreamstime.com/b/little-female-pomeranian-pet-show-dog-22237703.jpg" className="profile-picture img-circle"></img>
+        <div className="profile-picture col-md-4 col-sm-4 col-lg-4">
+        <img src="https://thumbs.dreamstime.com/b/little-female-pomeranian-pet-show-dog-22237703.jpg" className="profile-picture img-circle waves-effect waves-light"></img>
         </div>
       </div>
       <div className="row">
         <div className="event-info col-md-12 col-sm-12 col-lg-12">
-        <span>EVENT INFO LORUS IPSOM SHIGGY DIGGY LATIN</span>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phallus condimentum, sem non tincidunt suscipit, tortor sapien sodales magna, eget varius felis sem at ante. Sed ut mollis nisi, nec ultrices enim. Morbi sit amet ullamcorper.</span>
         </div>
       </div>
     </div>
