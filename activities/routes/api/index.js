24643bcd5c42bb74569ef709d114bb7ea5f4b8d0
ksbains <k1bains@ucsd.edu>
2018-01-08 20:28:21 -0800

    starting the routes and starting up the database

--- activities/routes/api/index.js
+++ activities/routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const activityRoutes = require("./activity");
 
-//Activity Routes
-router.use ("./activity", activityRoutes);
+
+const activitiesRoutes = require("./activity");
+const userRoutes = require('./user');
+
+//Article Routes
+router.use ("/activities", activitiesRoutes);
+router.use('/users', userRoutes);
+
 
 module.exports = router;
