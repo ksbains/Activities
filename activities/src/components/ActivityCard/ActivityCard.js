0caf48779b92771f8858f628daf4c6854aa04259
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-08 17:42:03 -0800

    with branch

--- activities/src/components/ActivityCard/ActivityCard.js
+++ activities/src/components/ActivityCard/ActivityCard.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React from 'react';
 import "./ActivityCard.css";
 
 
 // TODO: CHANGE CSS FOR LINKS & LINKS HOVER ...ect
 
 // TODO: USER ABLE TO CHOOSE BACKGROUND COLOR FOR .USER-CREATOR-INFO
+
+// TODO: INCREASE FONT SIZE FLAKE SCORE NUMBER/DECREASE FONT SIZE "FLAKE SCORE" TEXT
+
+
 
 class ActivityCard extends React.Component {
 render () {
