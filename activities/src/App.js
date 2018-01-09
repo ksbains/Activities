798d8af9d16d4896f48590745c806ec4777ce126
yaw owusu <asirifiyaw@gmail.com>
2018-01-08 18:34:18 -0800

    Added a googleMap component

--- activities/src/App.js
+++ activities/src/App.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
-import ActivityCard from "./components/ActivityCard"
+import ActivityCard from "./components/ActivityCard";
+import EventDescription from "./components/EventDescription";
+import GoogleMap from "./components/GoogleMap";
+
+
 class App extends Component {
  render() {
    return (
      <div className="App">
      <Navbar/>
      <ActivityCard/>
+     <EventDescription/>
+     <GoogleMap/>
+    
      </div>
    );
  }
 }
+
+
+
 
 export default App;
