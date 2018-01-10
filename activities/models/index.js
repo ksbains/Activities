a9249f40ec3dc5c87208dadc04c9e79a71857846
ksbains <k1bains@ucsd.edu>
2018-01-09 19:41:25 -0800

    added the serviceProviders

--- activities/models/index.js
+++ activities/models/index.js
@@ -1,10 +1,6 @@
 var goosestrap = require('goosestrap');
 var path = require('path');
 
-var db = goosestrap("mongodb://localhost/Activities", path.resolve("./models/*"));
-
-var Comment = db.model('Comment');
-var User = db.model('User');
-var Activity = db.model('Activity');
+var db = goosestrap("mongodb://localhost/Activities", path.resolve("../models/*"));
 
 module.exports = db;
--- activities/src/providers/ActivityService.js
+++ activities/src/providers/ActivityService.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all activities
+  getActivities: function() {
+    return axios.get("/api/activities");
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getActivity: function(id) {
+    return axios.get("/api/activities/" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteActivity: function(id) {
+    return axios.delete("/api/activities/" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveActivity: function(bookData) {
+    return axios.post("/api/activities", bookData);
   }
 };
