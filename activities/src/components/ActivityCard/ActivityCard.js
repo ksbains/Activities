cfb4676b42853518274b1defa2bb097c675e9611
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-19 14:55:43 -0800

    able to retrieve target data onclick for Activity Cards

--- activities/src/components/ActivityCard/ActivityCard.js
+++ activities/src/components/ActivityCard/ActivityCard.js
@@ -14,27 +14,6 @@ export class ActivityCard extends React.Component{
     <div className="activity-card-area">
       <div className="activity-card-container container">
         <div className="row">
-         {/*} <div className="col-sm-8 col-md-8 col-lg-8">
-            <div className="event-substance">
-              <div className="event-activity">
-                <strong><a href="#"> {this.props.activityType} </a></strong>
-              </div>
-              <div className="event-info">
-                <p className="card-text">{this.props.description}</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4 col-md-4 col-lg-4 user-creator-info">
-              <div className="user-created">     
-                <strong>{this.props.username}</strong>
-              </div>
-              <div className="user-profile-picture">
-                <img src="https://thumbs.dreamstime.com/b/little-female-pomeranian-pet-show-dog-22237703.jpg" className="user-profile-picture img-circle"></img>
-              </div>
-              <div className="user-flake-score">
-                <strong>{this.props.flakeScore}</strong>
-              </div>
-          </div> */}
             { this.props.children }
         </div>
       </div>
