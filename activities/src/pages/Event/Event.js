278a9d55c10e461d2c34bfe74d625c13525371de
ksbains <k1bains@ucsd.edu>
2018-01-19 18:58:57 -0800

    heroku times

--- activities/src/pages/Event/Event.js
+++ activities/src/pages/Event/Event.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import Navbar from "../../components/Navbar";
 import User from "../../components/User";
 import EventDescription from "../../components/EventDescription";
-import GoogleMap from "../../components/GoogleMap";
-import { Comment, CommentBox, CommentForm, CommentList, Data } from "../../components/Chat";
+// import GoogleMap from "../../components/GoogleMap";
 import "./Event.css";
 
 export class EventPage extends Component {
@@ -19,22 +18,13 @@ export class EventPage extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-5"> 
-						<User/>
+						<User />
 						</div>
 					</div>
-					<div className="row">
-						<div className="col-md-5"> 
-						<GoogleMap />
-						</div>
-					</div>
+
 					<div className="row">
 						<div className="col-md-5">
 						<EventDescription/>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-md-5">
-						<Comment/>
 						</div>
 					</div>
 				</div>
