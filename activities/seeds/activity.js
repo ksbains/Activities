53de424ba42a490dfd714673b46e986702d579f7
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-20 12:57:32 -0800

    updated with master

---
+++ activities/seeds/activity.js
@@ -0,0 +1,151 @@
+module.exports = activitySeed = {
+[{
+            address: "Petco Park",
+            time: "March 24, 2018",
+            duration: "1 hour",
+            activityType: "Padres Game",
+            fam: true,
+            maxPeople: 6,
+            description: "Bring the heat! Go Padres!!",
+            reoccuring: true
+            creator: {
+                first: "Mohammed",
+                last: "Avdol",
+                username: "MagiciansRed",
+                email: "mavdol@gmail.com",
+                password: "password",
+                location: "Egypt",
+                phoneNumber: "1234567",
+                flakeScore: "0.01",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+            }
+        },
+        {
+            address: "Yo Mama's House",
+            time: "April 20, 2018",
+            duration: "1 hour",
+            activityType: "Party",
+            fam: true,
+            maxPeople: 10,
+            description: "Yo mama so fat she brought a spoon to the super bowl!!",
+            reoccuring: true
+            creator: {
+                first: "Polnareff",
+                last: "Jean",
+                username: "SilverChariot",
+                email: "pjean@gmail.com",
+                password: "password",
+                location: "France",
+                phoneNumber: "1234567",
+                flakeScore: "0.2",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+            }
+        },
+        {
+            address: "Central Park",
+            time: "April 20, 2015",
+            duration: "6 hours",
+            activityType: "Baseball",
+            fam: true,
+            maxPeople: 17,
+            description: "The American Past Time!!",
+            reoccuring: true
+                creator: {first: "Kakyoin",
+                last: "Noriaki",
+                username: "HeirophantGreen",
+                email: "knoriaki@gmail.com",
+                password: "password",
+                location: "Japan",
+                phoneNumber: "1234567",
+                flakeScore: "0.03",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+           }
+        },
+        {
+            address: "Larry's Crib",
+            time: "April 20, 2018",
+            duration: "1 hour",
+            activityType: "Drinking and Thats it!",
+            fam: true,
+            maxPeople: 9,
+            description: "Bring Individuals of the fun variety!!",
+            reoccuring: true,
+            creator: {
+                first: "Joseph",
+                last: "Joestar",
+                username: "PurpleHermit",
+                email: "jjoestar@gmail.com",
+                password: "password",
+                location: "England",
+                phoneNumber: "1234567",
+                flakeScore: "0.04",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+            }
+        },
+        {
+            address: "Downtown, SD - Whaley House",
+            time: "April 20, 2018",
+            duration: "1 hour",
+            activityType: "Movie Night!",
+            fam: true,
+            maxPeople: 5,
+            description: "Whoo Baby! This is gonna be scary as *heck*!",
+            reoccuring: true
+            creator: {
+                first: "Jotaro",
+                last: "Joestar",
+                username: "StarPlatinum",
+                email: "jutes@gmail.com",
+                password: "password",
+                location: "Japan",
+                phoneNumber: "1234567",
+                flakeScore: "0.05",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+            }
+        },
+        {
+            address: "4432 UTC Mall - Ice Rink",
+            time: "April 20, 2018",
+            duration: "1 hour",
+            activityType: "Ice Skating",
+            fam: true,
+            maxPeople: 2,
+            description: "Dont forget your ice skates ; )!",
+            reoccuring: true
+            creator: {
+                first: "Iggy",
+                last: "Pop",
+                username: "ZaFool",
+                email: "iggy@gmail.com",
+                password: "password",
+                location: "New York",
+                phoneNumber: "1234567",
+                flakeScore: "0.06",
+                bio: "test",
+                activitiesCreated: [],
+                activitiesJoined: [],
+                lastLogin: "",
+                status: "test"
+            }
+    }]
+}
---
+++ activities/seeds/comment.js
@@ -0,0 +1,7 @@
+module.exports = commentSeed = {
+	[{
+        author: "Dio Brando",
+        message: "How many loaves of bread do we need?",
+        timeStamp: "10:10"
+}]
+}
