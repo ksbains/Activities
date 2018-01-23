7f357feeeb76bb6644cccf9bd38a5a4f28c00bd7
JuliusDorfman <juliusdorfman@gmail.com>
2018-01-22 21:28:02 -0800

    pushing collections to homepage

--- activities/src/components/ActivityCard/ActivityCard.js
+++ activities/src/components/ActivityCard/ActivityCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import "./ActivityCard.css";
 
 // TODO: CHANGE CSS FOR LINKS & LINKS HOVER ...ect
--- activities/src/pages/Homepage/Homepage.js
+++ activities/src/pages/Homepage/Homepage.js
@@ -3,6 +3,7 @@ import "./Homepage.css";
 import Navbar from "../../components/Navbar/Navbar.js";
 import { ActivityCard, ActivityCardUserInfo, ActivityCardEventInfo, ActivityCardWrapper } from "../../components/ActivityCard";
 import ActivityService from "../../providers/ActivityService.js";
+import UserService from "../../providers/UserService.js";
 
 // TODO: CHANGE LOOP LENGTH TO NUBER OF EVENTS IN DATABASE. 
 // USE .MAP HIGH FUNCTIONS
@@ -15,24 +16,57 @@ class Homepage extends Component {
     super(props);
     this.expandEventCard = this.expandEventCard.bind(this);
     this.state = {
-		activities: []
+		activities: [],
+		users: [],
+		combined: []
 	};
   }
 
-	componentDidMount = () => {
+	componentWillMount = () => {
 		console.log("Load Homepage")
 		this.loadEvents();
 	};
 
 	loadEvents = () => {
+		var activitiesArray = [];
+		var usersArray = [];
+		var combinedArray = [];
 		console.log("Load Events");
 		ActivityService.getActivities()
-			.then(res => this.setState({ activities: res.data }))
+			.then(res => {
+				this.setState({ activities: res.data })
+				for (let i = 0; i < res.data.length; i++) {
+					activitiesArray.push(res.data[i]);
+					}
+			UserService.getUsers()
+			.then(res2 => {
+				this.setState({ users: res2.data })
+				for (let j = 0; j < res2.data.length; j++) {
+					usersArray.push(res2.data[j]);
+					}
+
+				for (let k = 0; k < activitiesArray.length; k++) {	
+					var tempObject = {};
+					tempObject.activity = activitiesArray[k]
+					tempObject.user = usersArray[k]
+					combinedArray.push(tempObject)
+				}
+				this.setState({ combined: combinedArray})
+			})
+				.catch(err => console.log("error", err));
+		})
 			.catch(err => console.log("error", err));
+		
 	};
+
+
 
 	expandEventCard = (index) => {
 		console.log("this-expandEventCard", this.state.activities[index])
+	};
+
+	expandUserInfo = (index) => {
+		console.log("this-expandUserCard", this.state.users[index])
 	};
 
 	filterActivity = (index) => {
@@ -44,24 +78,26 @@ class Homepage extends Component {
 			<div>
 				<Navbar/>
 				<ActivityCardWrapper>
-				{this.state.activities.map((activity, index) => {
+				{this.state.combined.map((object, index) => {
 					return(
 						<ActivityCard
-						id = {activity._id}
-						activityType = {activity.activityType}
-						description = {activity.description}
-						username = {activity.username}
-						flakeScore = {activity.flakeScore}
+						id = {object.activity._id}
+						activityType = {object.activity.activityType}
+						description = {object.activity.description}
+						username = {object.user.username}
+						flakeScore = {object.user.flakeScore}
+
 						>
 							<ActivityCardEventInfo
 							onClickExpandEventCard = {this.expandEventCard.bind(this, index)}
 							onClickFilterActivity = {this.filterActivity.bind(this, index)}
-							activityType = {activity.activityType}
-							description = {activity.description}
+							activityType = {object.activity.activityType}
+							description = {object.activity.description}
 							/>
 							<ActivityCardUserInfo
-							username = {activity.username}
-							flakeScore = {activity.flakeScore}
+							onClickExpandUserInfo = {this.expandUserInfo.bind(this, index)}
+							username={object.user.username}
+							flakeScore={object.user.flakeScore}
 							/>							
 						</ActivityCard>
 				);
