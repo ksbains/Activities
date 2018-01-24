ea96d930b46ed19a1d68eaf1ce070e8f97b8350c
yaw owusu <asirifiyaw@gmail.com>
2018-01-23 20:30:49 -0800

    tidied up event page

--- activities/src/components/EventDescription/EventDescription.jsx
+++ activities/src/components/EventDescription/EventDescription.jsx
@@ -2,21 +2,16 @@ import React from "react";
 import "./EventDescription.css";
 
 
-class EventDescription extends React.Component{
 
-
-	render(){
-		return(
-		<div className="container">
+const EventDescription = () =>{
+	return(
+		<div className="container" id="eventDescription">
            <div className="panel panel-default" id="containerED">
-            <div className="panel-heading">Event Description</div>
+            <div className="panel-heading"><h4>Event Description</h4></div>
             <div className="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porta varius enim, ut lacinia nisl bibendum ut. Donec viverra sodales.</div>
           </div>
         </div>
-		)
 
-	}
-
+	)
 }
-
 export default EventDescription;
